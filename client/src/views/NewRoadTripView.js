4b01c8ca29f7740bcbfcb50e2a72dafe48020208
XueqiHuang <hxueqitao@gmail.com>
2022-11-14 09:52:26 +0100

    views folder done

---
+++ client/src/views/NewRoadTripView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewRoadTripView() {
+  return (
+    <div className="NewRoadTripView">
+      <h1>Hello world! I'm coming...</h1>
+    </div>
+  );
+}
+
+export default NewRoadTripView;
