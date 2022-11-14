6bc3b60d6e83c4379c049538068c43a142702797
XueqiHuang <hxueqitao@gmail.com>
2022-11-14 17:34:09 +0100

    add userview, loginview, membersonly view, privateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+
+import Local from "./helpers/Local";
+import Api from "./helpers/Api";
+
 import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+
+import PrivateRoute from "./components/PrivateRoute";
+import LoginView from "./views/LoginView";
 import HomeView from "./views/HomeView";
 // import FeaturedTripView from "./views/FeaturedTripView";
 // import NewRoadTripView from "./views/NewRoadTripView";
 // import PastFormView from "./views/PastFormView";
+import MembersOnlyView from "./views/MembersOnlyView";
+import UsersView from "./views/UsersView";
 import ProfileView from "./views/ProfileView";
 import Error404View from "./views/Error404View";
 
-import "./App.css";
-
 function App() {
+  const [user, setUser] = useState(Local.getUser());
+  const [loginErrorMsg, setLoginErrorMsg] = useState("");
+  const navigate = useNavigate();
+
+  async function doLogin(username, password) {
+    let myresponse = await Api.loginUser(username, password);
+    if (myresponse.ok) {
+      Local.saveUserInfo(myresponse.data.token, myresponse.data.user);
+      setUser(myresponse.data.user);
+      setLoginErrorMsg("");
+      navigate("/");
+    } else {
+      setLoginErrorMsg("Login failed");
+    }
+  }
+
+  function doLogout() {
+    Local.removeUserInfo();
+    setUser(null);
+    // (NavBar will send user to home page)
+  }
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar user={user} logoutCb={doLogout} />
       <p>Road Triper</p>
 
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="profile" element={<ProfileView />} />
+        <Route path="/users" element={<UsersView />} />
+        <Route
+          path="/users/:userId"
+          element={
+            <PrivateRoute>
+              <ProfileView />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/members-only"
+          element={
+            <PrivateRoute>
+              <MembersOnlyView />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <LoginView
+              loginCb={(u, p) => doLogin(u, p)}
+              loginError={loginErrorMsg}
+            />
+          }
+        />
         <Route path="*" element={<Error404View />} />
       </Routes>
     </div>
