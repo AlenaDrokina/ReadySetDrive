b24fce3208f2e3a4d8c4b1f6b7065c7ddfe3da82
XueqiHuang <hxueqitao@gmail.com>
2022-11-14 13:32:39 +0100

    Navbar and put routes into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomeView from "./views/HomeView";
+// import FeaturedTripView from "./views/FeaturedTripView";
+// import NewRoadTripView from "./views/NewRoadTripView";
+// import PastFormView from "./views/PastFormView";
+import ProfileView from "./views/ProfileView";
+import Error404View from "./views/Error404View";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>This si app</p>
+      <Navbar />
+      <p>Road Triper</p>
+
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="profile" element={<ProfileView />} />
+        <Route path="*" element={<Error404View />} />
+      </Routes>
     </div>
   );
 }
