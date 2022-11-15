80c0ede4b6c4a0c16a65ed9d65dd535a75f9839c
cecelia001 <diascece@gmail.com>
2022-11-15 11:26:28 +0100

    No error messages

--- client/src/components/MarkerTable.js
+++ client/src/components/MarkerTable.js
@@ -1,27 +1,27 @@
-// import React from "react";
-// // import "./MarkerTable.css";
+import React from "react";
+// import "./MarkerTable.css";
 
-// function MarkerTable(props) {
-//   return (
-//     <table className="MarkerTable-table">
-//       <thead>
-//         <tr>
-//           <th>Address</th>
-//           {/* <th>Formatted Address (from OpenCage)</th>
-//           <th>Latitude/Longitude</th> */}
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {props.places.map((p) => (
-//           <tr key={p.name}>
-//             <td>{p.name}</td>
-//             {/* <td>{p.formatted_address}</td>
-//             <td>{p.latLng.join("/")}</td> */}
-//           </tr>
-//         ))}
-//       </tbody>
-//     </table>
-//   );
-// }
+function MarkerTable(props) {
+  return (
+    <table className="MarkerTable-table">
+      <thead>
+        <tr>
+          <th>Address</th>
+          {/* <th>Formatted Address (from OpenCage)</th>
+          <th>Latitude/Longitude</th> */}
+        </tr>
+      </thead>
+      <tbody>
+        {props.places.map((p) => (
+          <tr key={p.name}>
+            <td>{p.name}</td>
+            {/* <td>{p.formatted_address}</td>
+            <td>{p.latLng.join("/")}</td> */}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
-// export default MarkerTable;
+export default MarkerTable;
