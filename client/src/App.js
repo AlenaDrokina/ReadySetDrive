7bad3fdcf4c1b9992335b6751e3bfaea0416a411
L-Pipo <lea.deborah.pipo@gmail.com>
2022-11-15 13:16:03 +0100

    Fetched roadtrip data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import HomeView from "./views/HomeView";
@@ -9,12 +9,29 @@ import ProfileView from "./views/ProfileView";
 import Error404View from "./views/Error404View";
 // import Local from "./helpers/Local";
 
-import TheMap from "./components/TheMap"
+import TheMap from "./components/TheMap";
 
 import "./App.css";
 
+import Api from "./helpers/Api";
+
 function App() {
   // const [user, setUser] = useState(Local.getUser());
+
+  let [roadtripData, setRoadtripData] = useState([]);
+
+  useEffect(() => {
+    fetchRoadtrips();
+  }, []);
+
+  async function fetchRoadtrips() {
+    let myresponse = await Api.getRoadtrips();
+    if (myresponse.ok) {
+      setRoadtripData(myresponse.data);
+    } else {
+      console.log("Response not okay.");
+    }
+  }
 
   return (
     <div className="App">
@@ -22,7 +39,7 @@ function App() {
       <p>Road Tripper</p>
 
       <Routes>
-        <Route path="/" element={<HomeView />} />
+        <Route path="/" element={<HomeView roadtripData={roadtripData} />} />
         <Route path="/profile" element={<ProfileView />} />
         <Route path="*" element={<Error404View />} />
         <Route path="/pastForm" element={<PastFormView />} />
---
+++ client/src/components/RoadtripCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RoadtripCard(props) {
+  return <div>{props.roadtripData.title}</div>;
+}
+
+export default RoadtripCard;
