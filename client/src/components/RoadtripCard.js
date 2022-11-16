66125771af96871c531b294fa1e022d0d7c7acee
L-Pipo <lea.deborah.pipo@gmail.com>
2022-11-16 12:32:04 +0100

    Finished roadtrip cards and started to work on Home View

--- client/src/components/RoadtripCard.js
+++ client/src/components/RoadtripCard.js
@@ -1,11 +1,21 @@
 import React from "react";
+import "./RoadtripCard.css";
 
 function RoadtripCard(props) {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <div>{props.roadtripData.title}</div>
-      <div>{props.roadtripData.countries}</div>
-      <div>{props.roadtripData.description}</div>
+    <div className="col-md-6 col-lg-4 mb-4">
+      <div className="card">
+        <img
+          className="card-img-top"
+          src={props.roadtripData.image_url}
+          alt="roadtrip"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{props.roadtripData.title}</h5>
+          <h6 className="card-text">{props.roadtripData.countries}</h6>
+          <p className="card-text">{props.roadtripData.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,13 +1,31 @@
 import React from "react";
 import RoadtripCard from "../components/RoadtripCard";
+import "./HomeView.css";
 
 function Homeview(props) {
   return (
     <div className="Homeview">
-      <h1>Hello world! I'm coming in a minute...</h1>
-      {props.roadtripData.map((element) => {
-        return <RoadtripCard key={element.id} roadtripData={element} />;
-      })}
+      <div className="container">
+        <div className="row height justify-content-center align-items-center">
+          <div className="col-md-6">
+            <div className="form">
+              <input
+                type="text"
+                className="form-control form-input"
+                placeholder="Search country..."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {props.roadtripData.map((element) => {
+            return <RoadtripCard key={element.id} roadtripData={element} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
