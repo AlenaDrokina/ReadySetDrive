5ec6a61af989990f84d49cf86b534691fddeace1
XueqiHuang <hxueqitao@gmail.com>
2022-11-21 15:20:05 +0100

    register styling

--- client/src/views/RegisterView.js
+++ client/src/views/RegisterView.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./RegisterView.css";
 
 const RegisterView = (props) => {
   const [username, setUserName] = useState("");
@@ -19,71 +20,88 @@ const RegisterView = (props) => {
   const gotoLoginPage = () => navigate("/login");
 
   return (
-    <div className="signup__container">
-      <div className="col-4 offset-4">
-        <h2>Sign up </h2>
-        <form className="signup__form" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+    <div className="container-fluid">
+      <div className="row main-content bg-success text-center">
+        <div className="col-md-4 text-center company__info">
+          <span className="company__logo">
+            <h2>
+              <span className="fa fa-android"></span>
+            </h2>
+          </span>
+          <h4 className="company_title">Your Company Logo</h4>
+        </div>
+        <div className="col-md-8 col-xs-12 col-sm-12 signup__form ">
+          <div className="container-fluid">
+            <div className="row">
+              <h2>Register</h2>
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={username}
-              required
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
+            <div className="row">
+              <form control="" className="signup__form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email">Email Address</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
 
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              minLength={6}
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    required
+                    onChange={(e) => setUserName(e.target.value)}
+                  />
+                </div>
 
-          <div className="form-group">
-            <label htmlFor="confpassword">Confirm Password</label>
-            <input
-              type="password"
-              name="confPassword"
-              id="confPassword"
-              minLength={6}
-              required
-              value={confPassword}
-              onChange={(e) => setConfPassword(e.target.value)}
-            />
-          </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    minLength={6}
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
 
-          <button type="submit" className="btn btn-primary">
-            SIGN UP
-          </button>
-          <p>{props.registerError}</p>
-          <p>
-            Already have an account?{" "}
-            <span className="text-success" onClick={gotoLoginPage}>
-              Login
-            </span>
-          </p>
-        </form>
+                <div className="form-group">
+                  <label htmlFor="confpassword">Confirm Password</label>
+                  <input
+                    type="password"
+                    name="confPassword"
+                    id="confPassword"
+                    minLength={6}
+                    required
+                    value={confPassword}
+                    onChange={(e) => setConfPassword(e.target.value)}
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+                <p>{props.registerError}</p>
+                <p>
+                  Already have an account?{" "}
+                  <span className="text-success" onClick={gotoLoginPage}>
+                    Login
+                  </span>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
